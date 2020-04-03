29813173f795a644dd3fc075e41bab84d595d104
Shikhar <shicha1203@tamu.edu>
2020-04-02 23:53:30 -0500

    updated util

---
+++ util/database.js
@@ -0,0 +1,27 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = callback => {
+    MongoClient.connect('mongodb+srv://shicha:AggieRent@123@cluster0-mkcgq.mongodb.net/shop?retryWrites=true&w=majority').then(client => {
+        console.log('connected');
+        _db = client.db();
+        callback(client);
+    })
+    .catch(err => {
+        console.log(err);
+    });  
+};
+
+const getDb = () => {
+    if (_db){
+        return _db;
+    }
+    throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+
+
